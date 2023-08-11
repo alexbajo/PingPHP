3d2b036c9ccfb828acf90ac6d7aee2922532cfdd
Alejandro Bajo <alejandrobajo@proturhotels.com>
2023-08-11 13:06:41 +0200

    colores

--- index.php
+++ index.php
@@ -1,22 +1,45 @@
 <?php 
 
-
-function ping($ip_addr){
-        echo "<br>";
-      if (!exec("ping -n 1 -w 1 ".$ip_addr." 2>NUL > NUL && (echo 0) || (echo 1)")){
-                echo "El host existe -" . $ip_addr;
-                return true;
-        } else {
-            echo "El host no existe -" . $ip_addr;
-            return false;
-        }
-
+function ping($ip_addr) {
+    if (!exec("ping -n 1 -w 1 " . $ip_addr . " 2>NUL > NUL && (echo 0) || (echo 1)")) {
+        return "<span style='color:green'>" . $ip_addr . "</span>";
+    } else {
+        return "<span style='color:red'>" . $ip_addr . "</span>";
     }
-    
+}
 
-    $ip_addr3 = "10.100.21.11";
+// Si se envía una IP a agregar.
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ip_to_add'])) {
+    $newIp = filter_var($_POST['ip_to_add'], FILTER_VALIDATE_IP);
+    if ($newIp) {
+        file_put_contents('devices.txt', $newIp . "\n", FILE_APPEND);
+    }
+}
 
-ping($ip_addr3);
-$ip_addr2 = "10.100.21.1";
+// Si se envía una IP a eliminar.
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ip_to_delete'])) {
+    $ipToDelete = $_POST['ip_to_delete'];
+    $ipAddresses = file('devices.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    if (($key = array_search($ipToDelete, $ipAddresses)) !== false) {
+        unset($ipAddresses[$key]);
+        file_put_contents('devices.txt', implode("\n", $ipAddresses));
+    }
+}
 
-ping($ip_addr2);
+// Leemos las IPs desde el archivo.
+$ipAddresses = file('devices.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+?>
+
+<form method="POST" action="">
+    <input type="text" name="ip_to_add" placeholder="Introduce una IP">
+    <input type="submit" value="Añadir IP">
+</form>
+
+<?php
+// Hacemos ping a cada IP y mostramos un botón para eliminarla.
+foreach ($ipAddresses as $ip) {
+    echo ping($ip);
+    echo " <form method='POST' action='' style='display:inline;'><input type='hidden' name='ip_to_delete' value='{$ip}'><input type='submit' value='Borrar'></form><br>";
+}
+?>
